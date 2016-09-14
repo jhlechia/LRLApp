23125301083ba22cfc74dd32c1a5ce1c42f40006
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-09-14 15:36:49 -0400

    move jsx to lrl.js

---
+++ src/lrl.js
@@ -0,0 +1,14 @@
+var SplashPage = React.createClass({
+  render: function(){
+    recent (
+      <div>
+        <h3>Welcome to Ladies Room Live!</h3>
+        <h1>Our next event is (countdown)</h1>
+        <h1>Join Us(button)</h1>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<SplashPage />, document.getElementById('container'));
+</script>
